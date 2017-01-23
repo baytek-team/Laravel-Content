9c6c9d859104126dc6ac711338052aa51ea9d134
Sarah Marinoff <sarah@baytek.ca>
2017-01-23 14:25:18 -0500

    Fixed up the content create view to match the edit view.

--- resources/Views/content/create.blade.php
+++ resources/Views/content/create.blade.php
@@ -1,14 +1,33 @@
 @extends('Pretzel::admin')
 @section('content')
 
+<div class="ui two column stackable grid">
+    <div class="ten wide column">
+        <h1 class="ui header">
+            <i class="user icon"></i>
+            <div class="content">
+                Content Management
+                <div class="sub header">Create the content for the application.</div>
+            </div>
+        </h1>
+    </div>
+</div>
+
+<div class="ui hidden divider"></div>
+
 <div class="flex-center position-ref full-height">
     <div class="content">
-        <form action="{{action('\Baytek\LaravelContent\Controllers\ContentController@store')}}" method="POST">
+        <form action="{{action('\Baytek\LaravelContent\Controllers\ContentController@store')}}" method="POST" class="ui form">
             {{ csrf_field() }}
 
             @include('Pretzel::content.form')
 
-            <input type="submit" value="Save all the things">
+            <div class="field actions">
+	            <a class="ui button" href="{{ action('\Baytek\LaravelContent\Controllers\ContentController@index') }}">Cancel</a>
+	            <button type="submit" class="ui right floated primary button">
+	            	Save all the things
+            	</button>
+            </div>
         </form>
     </div>
 </div>
