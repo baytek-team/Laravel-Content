af1eb538c665576d1cd8a0dce54c4b5bdb5478e5
Yvon Viger <yvon@baytek.ca>
2017-03-13 08:34:14 -0400

    Adding localization stuff

---
+++ src/localization.php
@@ -0,0 +1,19 @@
+<?php
+
+dump(function_exists('__'));
+
+if (! function_exists('__')) {
+    /**
+     * Translate the given message.
+     *
+     * @param  string  $key
+     * @param  array  $replace
+     * @param  string  $locale
+     * @return \Illuminate\Contracts\Translation\Translator|string
+     */
+    function __($key = null, $replace = [], $locale = null)
+    {
+        dd($key);
+        return app('translator')->getFromJson($key, $replace, $locale);
+    }
+}
