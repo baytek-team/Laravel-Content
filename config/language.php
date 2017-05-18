ef67544c8936a0f90c2b7e79b80468bf36b3ca8b
Chad Sehn <chad@baytek.ca>
2017-05-18 11:41:53 -0400

    Adding in the config settings for lc_all for PHP

--- config/language.php
+++ config/language.php
@@ -6,6 +6,11 @@ return [
         'fr' => env('APP_FR_DOMAIN', 'french.domain'),
     ],
 
+    'lc_all' => [
+        'en' => env('APP_LC_ALL_EN', 'en_US'),
+        'fr' => env('APP_LC_ALL_FR', 'fr_FR'),
+    ],
+
     'locales' => [
     	'en', 'fr'
     ]
