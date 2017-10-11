0e341758eca225fd55674695afb992aa090214bf
Yvon Viger <yvon@bayteksystems.com>
2017-10-10 22:36:02 -0400

    Adding determineWhere to guess type and produce where or where in statements

--- src/Eloquent/Builder.php
+++ src/Eloquent/Builder.php
@@ -10,6 +10,7 @@ class Builder extends EloquentBuilder
 {
     /**
      * Get the hydrated models without eager loading.
+     * Build that model if its content type is set.
      *
      * @param  array  $columns
      * @return \Illuminate\Database\Eloquent\Model[]
@@ -19,10 +20,30 @@ class Builder extends EloquentBuilder
         $records = $this->query->get($columns);
 
         return $records->map(function($record) {
+            // Check if the content type is set, if so build that type.
             if(isset($record->content_type) && $record->content_type) {
                 return (new $record->content_type)->newFromBuilder($record);
             }
             return $this->model->newFromBuilder($record);
         })->all();
+    }
+
+    /**
+     * Determine which where to use with key value pair parameters
+     *
+     * @param  string $column
+     * @param  mixed $value
+     * @return Baytek\Laravel\Content\Eloquent\Builder
+     */
+    public function determineWhere($column, $value)
+    {
+        if(is_array($value)) {
+            $this->query->whereIn($column, $value);
+        }
+        else {
+            $this->query->where($column, $value);
+        }
+
+        return $this;
     }
 }
