d62ad0c74048ad06046e50d1588e7edc0e8f0041
Yvon Viger <yvon@baytek.ca>
2017-10-16 11:40:10 -0400

    Addressing path to status bit

--- src/ContentServiceProvider.php
+++ src/ContentServiceProvider.php
@@ -144,7 +144,7 @@ class ContentServiceProvider extends AuthServiceProvider
 
         $this->app->register(\Laracasts\Flash\FlashServiceProvider::class);
         $this->app->register(\Baytek\Laravel\Settings\SettingsServiceProvider::class);
-        $this->app->register(\Baytek\LaravelStatusBit\StatusBitServiceProvider::class);
+        $this->app->register(\Baytek\Laravel\StatusBit\StatusBitServiceProvider::class);
         $this->app->register(\Baytek\Laravel\Users\ServiceProvider::class);
         $this->app->register(\Baytek\Laravel\Menu\MenuServiceProvider::class);
     	$this->app->register(\Collective\Html\HtmlServiceProvider::class);
